d1b57141c172501e7618eb689baf4f5d990ab8b7
dannyleo16 <156039674+dannyleo16@users.noreply.github.com>
2024-01-12 12:40:03 -0500

    Add files via upload

--- modelo_distilbert.py
+++ modelo_distilbert.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/16JJe-2K--9Defs2y8ItH07vUGMTQfL3m
 """
 
+import time
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 import requests
@@ -18,13 +19,13 @@ model = AutoModelForTokenClassification.from_pretrained("dccuchile/distilbert-ba
 # Define una función de consulta
 API_URL = "https://api-inference.huggingface.co/models/dccuchile/distilbert-base-spanish-uncased-finetuned-ner"
 headers = {"Authorization": "Bearer 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"}
-
+start_time= time.time()
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
 # Carga el archivo csv
-df = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/ecuPrueba_Alertante1.csv')
+df = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/ecuPrueba_40.csv')
 
 # Itera sobre cada fila del dataframe
 for index, row in df.iterrows():
@@ -39,3 +40,6 @@ for index, row in df.iterrows():
         # Filtra por tipo 'LABEL_0'
         if entity['entity_group'] != 'LABEL_0':
             print(f"Entidad: {entity['word']}, Tipo: {entity['entity_group']}, Confianza: {entity['score']}")
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Tiempo de ejecución: {execution_time} segundos")
